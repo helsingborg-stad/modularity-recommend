239fa9c75b1c92b8aee67de104ce7e8556576a7d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-02-21 17:14:47 +0100

    fix: remove end / in meta

--- source/php/App.php
+++ source/php/App.php
@@ -58,7 +58,7 @@ class App
      */
     public function printMetaTag()
     {
-        echo '<meta name="rek_viewclick" content=""/>' . "\n";
+        echo '<meta name="rek_viewclick" content="">' . "\n";
     }
 
     /**
