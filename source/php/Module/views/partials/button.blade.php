a2d69ecaf2edb6cd00cd257ca1c443386687547e
William Lundqvist <william.lundqvist@consid.se>
2026-01-21 10:06:01 +0100

    Formatting

--- source/php/Module/views/partials/button.blade.php
+++ source/php/Module/views/partials/button.blade.php
@@ -1,16 +1,16 @@
 @button([
-'text' => $text,
-'style' => $style ?? 'outlined',
-'color' => $color ?? 'primary',
-'icon' => $icon ?? false,
-'href' => $href,
-'size' => 'sm',
-'context' => ['module.recommend', 'module.recommend.button'],
-'reversePositions' => $iconPosition == 'before' ? true : false,
-'classList' => [
-'recommend-linklist__item',
-'recommend-linklist__' . $type . '-item',
-'c-button--pill',
-],
+    'text' => $text,
+    'style' => $style ?? 'outlined',
+    'color' => $color ?? 'primary',
+    'icon' => $icon ?? false,
+    'href' => $href,
+    'size' => 'sm',
+    'context' => ['module.recommend', 'module.recommend.button'],
+    'classList' => [
+        'recommend-linklist__item',
+        'recommend-linklist__' . $type . '-item',
+        'c-button--pill',
+    ],
+    'reversePositions' => $iconPosition == 'before' ? true : false,
 ])
 @endbutton
